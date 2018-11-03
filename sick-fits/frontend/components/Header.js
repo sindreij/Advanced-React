86a6169024d14923870e9089d1180f4a16fef797
Sindre Johansen <sindre@iterate.no>
2018-11-03 15:26:37 +0100

    Stuff

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -1,7 +1,19 @@
 import Link from "next/link";
 import styled from "styled-components";
+import Router from "next/router";
+import NProgress from "nprogress";
 
 import Nav from "./Nav";
+
+Router.onRouteChangeStart = () => {
+    NProgress.start();
+};
+Router.onRouteChangeComplete = () => {
+    NProgress.done();
+};
+Router.onRouteChangeError = () => {
+    console.log("onRouteChangeError triggered");
+};
 
 const Logo = styled.h1`
     font-size: 4rem;
