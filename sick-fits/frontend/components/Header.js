ac25b0d6709514ca12142f3d661199c6afdcdb7d
Sindre Johansen <sindre@iterate.no>
2018-11-03 14:16:41 +0100

    More stuff

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -4,12 +4,11 @@ import styled from "styled-components";
 import Nav from "./Nav";
 
 const Logo = styled.h1`
-    font-size: 2rem;
+    font-size: 4rem;
     margin-left: 2rem;
     position: relative;
     z-index: 2;
     transform: skew(-7deg);
-
     a {
         padding: 0.5rem 1rem;
         background: ${props => props.theme.red};
@@ -17,7 +16,6 @@ const Logo = styled.h1`
         text-transform: uppercase;
         text-decoration: none;
     }
-
     @media (max-width: 1300px) {
         margin: 0;
         text-align: center;
